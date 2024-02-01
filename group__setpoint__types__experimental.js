47d63e6b95e112ff035e590da85e4852437a080a
bkueng <bkueng@users.noreply.github.com>
2024-02-01 08:21:19 +0000

    deploy: dcf4802984b48f95f9bfaca00ed55adc409219bf

--- group__setpoint__types__experimental.js
+++ group__setpoint__types__experimental.js
@@ -5,7 +5,7 @@ var group__setpoint__types__experimental =
       [ "~AttitudeSetpointType", "classpx4__ros2_1_1AttitudeSetpointType.html#a8589be88a1f7b08a59a27921d43a1871", null ],
       [ "desiredUpdateRateHz", "classpx4__ros2_1_1AttitudeSetpointType.html#a666ce9f420176e601256c397c90bb886", null ],
       [ "getConfiguration", "classpx4__ros2_1_1AttitudeSetpointType.html#aa1e01a708a2de026cd114db77c269b17", null ],
-      [ "update", "classpx4__ros2_1_1AttitudeSetpointType.html#ab28e36ed4e22dc7e786b963f871effa4", null ]
+      [ "update", "classpx4__ros2_1_1AttitudeSetpointType.html#a16b30bd9486c97fd64b6555bff958d98", null ]
     ] ],
     [ "RatesSetpointType", "classpx4__ros2_1_1RatesSetpointType.html", [
       [ "RatesSetpointType", "classpx4__ros2_1_1RatesSetpointType.html#a2b6c6df87a6c18d5c6e20ead9f959847", null ],
