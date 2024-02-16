adc25b60b94f48f66146d93987d84ab03a4aba1c
GuillaumeLaine <guillaume.william1@hotmail.com>
2024-02-16 15:33:28 +0100

    control: add global goto setpoint

--- px4_ros2_cpp/include/px4_ros2/control/setpoint_types/goto.hpp
+++ px4_ros2_cpp/include/px4_ros2/control/setpoint_types/goto.hpp
@@ -10,6 +10,7 @@
 #include <optional>
 
 #include <px4_ros2/common/setpoint_base.hpp>
+#include <px4_ros2/utils/geodesic.hpp>
 
 namespace px4_ros2
 {
@@ -51,6 +52,38 @@ private:
   rclcpp::Node & _node;
   rclcpp::Publisher<px4_msgs::msg::GotoSetpoint>::SharedPtr
     _goto_setpoint_pub;
+};
+
+class GotoGlobalSetpointType
+{
+public:
+  explicit GotoGlobalSetpointType(Context & context);
+
+  ~GotoGlobalSetpointType() = default;
+
+  /**
+   * @brief Go-to global setpoint update
+   *
+   * Unset optional values are not controlled
+   *
+   * @param global_position latitude [deg], longitude [deg], altitude AMSL [m]
+   * @param heading [rad] from North
+   * @param max_horizontal_speed [m/s] in NE-datum of NED earth-fixed frame
+   * @param max_vertical_speed [m/s] in D-axis of NED earth-fixed frame
+   * @param max_heading_rate [rad/s] about D-axis of NED earth-fixed frame
+   */
+  void update(
+    const Eigen::Vector3d & global_position,
+    const std::optional<float> & heading = {},
+    const std::optional<float> & max_horizontal_speed = {},
+    const std::optional<float> & max_vertical_speed = {},
+    const std::optional<float> & max_heading_rate = {}
+  );
+
+private:
+  rclcpp::Node & _node;
+  std::shared_ptr<MapProjection> _map_projection;
+  std::shared_ptr<GotoSetpointType> _goto_setpoint;
 };
 
 /** @}*/
