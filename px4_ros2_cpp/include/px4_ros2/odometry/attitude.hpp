302ab586759b66c81e2cfee1c7b421d6f62b5271
GuillaumeLaine <guillaume.william1@hotmail.com>
2024-02-26 15:11:11 +0100

    odometry: add OdometryAttitude class

---
+++ px4_ros2_cpp/include/px4_ros2/odometry/attitude.hpp
@@ -0,0 +1,71 @@
+/****************************************************************************
+ * Copyright (c) 2024 PX4 Development Team.
+ * SPDX-License-Identifier: BSD-3-Clause
+ ****************************************************************************/
+
+#pragma once
+
+#include <Eigen/Eigen>
+#include <px4_msgs/msg/vehicle_attitude.hpp>
+#include <px4_ros2/common/context.hpp>
+#include <px4_ros2/odometry/subscription.hpp>
+#include <px4_ros2/utils/geometry.hpp>
+
+namespace px4_ros2
+{
+/** \ingroup odometry
+ *  @{
+ */
+
+/**
+ * Provides access to the vehicle's attitude estimate
+ */
+class OdometryAttitude : public Subscription<px4_msgs::msg::VehicleAttitude>
+{
+public:
+  explicit OdometryAttitude(Context & context);
+
+  /**
+   * @brief Get the vehicle's attitude.
+   *
+   * @return the attitude quaternion
+   */
+  Eigen::Quaternionf attitude() const
+  {
+    const px4_msgs::msg::VehicleAttitude & att = last();
+    return Eigen::Quaternionf{att.q[0], att.q[1], att.q[2], att.q[3]};
+  }
+
+  /**
+   * @brief Get the vehicle's roll in extrinsic RPY order.
+   *
+   * @return the attitude roll in radians within [-pi, pi]
+   */
+  float roll() const
+  {
+    return quaternionToRoll(attitude());
+  }
+
+  /**
+   * @brief Get the vehicle's pitch in extrinsic RPY order.
+   *
+   * @return the attitude pitch in radians within [-pi, pi]
+   */
+  float pitch() const
+  {
+    return quaternionToPitch(attitude());
+  }
+
+  /**
+   * @brief Get the vehicle's yaw in extrinsic RPY order.
+   *
+   * @return the attitude yaw in radians within [-pi, pi]
+   */
+  float yaw() const
+  {
+    return quaternionToYaw(attitude());
+  }
+};
+
+/** @}*/
+} /* namespace px4_ros2 */
---
+++ px4_ros2_cpp/src/odometry/attitude.cpp
@@ -0,0 +1,19 @@
+/****************************************************************************
+ * Copyright (c) 2023 PX4 Development Team.
+ * SPDX-License-Identifier: BSD-3-Clause
+ ****************************************************************************/
+
+#include <px4_ros2/odometry/attitude.hpp>
+
+namespace px4_ros2
+{
+
+OdometryAttitude::OdometryAttitude(Context & context)
+: Subscription<px4_msgs::msg::VehicleAttitude>(context, "/fmu/out/vehicle_attitude")
+{
+  RequirementFlags requirements{};
+  requirements.attitude = true;
+  context.setRequirement(requirements);
+}
+
+} // namespace px4_ros2
