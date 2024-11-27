112d856ee159d46cc1e1ee9765a76d3a45dff916
bdilman <bertug@auterion.com>
2024-11-27 10:14:13 +0100

    add auto loiter mode, nav-state 4, to be replaced by an external mode

--- px4_ros2_cpp/include/px4_ros2/components/mode.hpp
+++ px4_ros2_cpp/include/px4_ros2/components/mode.hpp
@@ -84,6 +84,8 @@ public:
     px4_msgs::msg::VehicleStatus::NAVIGATION_STATE_AUTO_RTL;
   static constexpr ModeID kModeIDPrecisionLand =
     px4_msgs::msg::VehicleStatus::NAVIGATION_STATE_AUTO_PRECLAND;
+  static constexpr ModeID kModeIDLoiter =
+    px4_msgs::msg::VehicleStatus::NAVIGATION_STATE_AUTO_LOITER;  
 
   struct Settings
   {
