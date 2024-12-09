dfa917426fb7d8fd6f2b3278cadbb27d0b0b054f
RomanBapst <bapstroman@gmail.com>
2024-12-09 15:33:35 +0300

    added fw goto and lateral control setpoint type
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- px4_ros2_cpp/include/px4_ros2/control/setpoint_types/experimental/trajectory.hpp
+++ px4_ros2_cpp/include/px4_ros2/control/setpoint_types/experimental/trajectory.hpp
@@ -53,7 +53,7 @@ public:
   /**
    * @brief Position setpoint update.
    *
-   * The GotoSetpointType should be preferred.
+   * The FwGotoSetpointType should be preferred.
    *
    * @param position_ned_m [m] NED earth-fixed frame
    */
