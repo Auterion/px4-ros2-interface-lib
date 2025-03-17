745bb9aa407b660fc4df651b5f549ecebce75452
mahima-yoga <mahima@auterion.com>
2025-03-17 15:42:56 +0100

    vtol: remove hard coded values and combine into one function.

--- px4_ros2_cpp/src/control/vtol.cpp
+++ px4_ros2_cpp/src/control/vtol.cpp
@@ -5,6 +5,8 @@
 
  #include <px4_ros2/control/vtol.hpp>
  #include <px4_ros2/utils/message_version.hpp>
+ #include <rclcpp/rclcpp.hpp>
+
  
  using namespace std::chrono_literals;
  
@@ -14,61 +16,43 @@
  VTOL::VTOL(Context & context)
  : _node(context.node())
  {
-   _vehicle_command_pub = _node.create_publisher<px4_msgs::msg::VehicleCommand>(
-     context.topicNamespacePrefix() + "fmu/in/vehicle_command" + px4_ros2::getMessageNameVersion<px4_msgs::msg::VehicleCommand>(),
-     1);
-//    _last_update = _node.get_clock()->now();
+    _vehicle_command_pub = _node.create_publisher<px4_msgs::msg::VehicleCommand>(
+    context.topicNamespacePrefix() + "fmu/in/vehicle_command" + px4_ros2::getMessageNameVersion<px4_msgs::msg::VehicleCommand>(),
+    1);
+    _vehicle_status_sub  = _node.create_subscription<px4_msgs::msg::VehicleStatus>(
+    context.topicNamespacePrefix() + "fmu/out/vehicle_status" + px4_ros2::getMessageNameVersion<px4_msgs::msg::VehicleStatus>(),
+    rclcpp::QoS(10).best_effort(), 
+    [this](px4_msgs::msg::VehicleStatus::UniquePtr msg) {
+        _system_id = msg->system_id; 
+        _component_id = msg->component_id; 
+        _vehicle_type = msg->vehicle_type;
+
+    });
+
  }
  
- void VTOL::transitionFWtoMC()
- {
-   // Rate-limit to avoid spamming the FC with commands at high frequency
-//    const auto now = _node.get_clock()->now();
-//    if (now - _last_update > 100ms) {
-    //  _last_update = now;
  
-     // Send command, don't wait for ack
+ void VTOL::transition()
+ {
+
      px4_msgs::msg::VehicleCommand cmd;
      cmd.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_VTOL_TRANSITION; 
 
-     cmd.param1 = 3;
+     cmd.param1 = (_vehicle_type == px4_msgs::msg::VehicleStatus::VEHICLE_TYPE_ROTARY_WING) ?  px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_MC : px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_FW ;
+
      cmd.param2 = 0;
 
-     cmd.source_system = 1; 
-     cmd.target_system = 1; 
-     cmd.source_component = 1; 
-     cmd.target_component = 1; 
+     cmd.source_system = _system_id; 
+     cmd.target_system = _system_id; 
+     cmd.source_component = _component_id; 
+     cmd.target_component = _component_id; 
 
      cmd.timestamp = 0; // Let PX4 set the timestamp
 
      _vehicle_command_pub->publish(cmd);
    
  }
-
- void VTOL::transitionMCtoFW()
- {
-   // Rate-limit to avoid spamming the FC with commands at high frequency
-//    const auto now = _node.get_clock()->now();
-//    if (now - _last_update > 100ms) {
-    //  _last_update = now;
  
-     // Send command, don't wait for ack
-     px4_msgs::msg::VehicleCommand cmd;
-     cmd.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_VTOL_TRANSITION; 
-
-     cmd.param1 = 4;
-     cmd.param2 = 0;
-
-     cmd.source_system = 1; 
-     cmd.target_system = 1; 
-     cmd.source_component = 1; 
-     cmd.target_component = 1; 
-
-     cmd.timestamp = 0; // Let PX4 set the timestamp
-     
-     _vehicle_command_pub->publish(cmd);
-//    }
- }
  
  } // namespace px4_ros2
  
