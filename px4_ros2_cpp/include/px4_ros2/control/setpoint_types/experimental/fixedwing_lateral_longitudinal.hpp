8b266056000443e262e5154c8bf41bb2225e8a0c
mahima-yoga <mahima@auterion.com>
2025-04-07 16:45:44 +0200

    fw-ctrl: seperate update arguments into two structs.

--- px4_ros2_cpp/include/px4_ros2/control/setpoint_types/experimental/fixedwing_lateral_longitudinal.hpp
+++ px4_ros2_cpp/include/px4_ros2/control/setpoint_types/experimental/fixedwing_lateral_longitudinal.hpp
@@ -16,9 +16,12 @@
 
 namespace px4_ros2
 {
-/** \ingroup setpoint_types
+/** \ingroup setpoint_types_experimental
  *  @{
  */
+
+struct FwLateralLongitudinalSetpoint;
+struct FwControlLimits;
 
 /**
  * @brief Setpoint type for fixedwing control
@@ -38,21 +41,10 @@ public:
    * Unset optional values are not controlled
    *
    */
-  void update(
-    const float course_setpoint,
-    const float airspeed_direction,
-    const float lateral_acceleration_setpoint,
-    const float altitude_setpoint_msl,
-    const float height_rate_setpoint,
-    const float equivalent_airspeed_setpoint,
-    const std::optional<float> & min_pitch = {},
-    const std::optional<float> & max_pitch = {},
-    const std::optional<float> & min_throttle = {},
-    const std::optional<float> & max_throttle = {},
-    const std::optional<float> & max_lat_acc = {},
-    const std::optional<float> & target_climb_rate = {},
-    const std::optional<float> & target_sink_rate = {}
-  );
+
+  void update(const FwLateralLongitudinalSetpoint & setpoint, const FwControlLimits & limits);
+
+  void update(const FwLateralLongitudinalSetpoint & setpoint);
 
   float desiredUpdateRateHz() override {return 30.f;}
 
@@ -67,6 +59,104 @@ private:
     _lateral_control_limits_pub;
   rclcpp::Publisher<px4_msgs::msg::LongitudinalControlLimits>::SharedPtr
     _longitudinal_control_limits_pub;
+};
+
+struct FwLateralLongitudinalSetpoint
+{
+  std::optional<float> course;
+  std::optional<float> airspeed_direction;
+  std::optional<float> lateral_acceleration;
+  std::optional<float> altitude_msl;
+  std::optional<float> height_rate;
+  std::optional<float> equivalent_airspeed;
+
+  FwLateralLongitudinalSetpoint & withCourse(float course_sp)
+  {
+    course = course_sp;
+    return *this;
+  }
+
+  FwLateralLongitudinalSetpoint & withAirspeedDirection(float airspeed_direction_sp)
+  {
+    airspeed_direction = airspeed_direction_sp;
+    return *this;
+  }
+
+  FwLateralLongitudinalSetpoint & withLateralAcceleration(float lateral_acceleration_sp)
+  {
+    lateral_acceleration = lateral_acceleration_sp;
+    return *this;
+  }
+
+  FwLateralLongitudinalSetpoint & withAltitude(float altitude_sp)
+  {
+    altitude_msl = altitude_sp;
+    return *this;
+  }
+
+  FwLateralLongitudinalSetpoint & withHeightRate(float height_rate_sp)
+  {
+    height_rate = height_rate_sp;
+    return *this;
+  }
+
+  FwLateralLongitudinalSetpoint & withEquivalentAirspeed(float equivalent_airspeed_sp)
+  {
+    equivalent_airspeed = equivalent_airspeed_sp;
+    return *this;
+  }
+};
+
+struct FwControlLimits
+{
+  std::optional<float> min_pitch;
+  std::optional<float> max_pitch;
+  std::optional<float> min_throttle;
+  std::optional<float> max_throttle;
+  std::optional<float> min_equivalent_airspeed;
+  std::optional<float> max_equivalent_airspeed;
+  std::optional<float> max_lateral_acceleration;
+  std::optional<float> target_climb_rate;
+  std::optional<float> target_sink_rate;
+
+  FwControlLimits & withPitchLimits(float min_pitch_sp, float max_pitch_sp)
+  {
+
+    min_pitch = min_pitch_sp;
+    max_pitch = max_pitch_sp;
+
+    return *this;
+  }
+
+  FwControlLimits & withThrottleLimits(float min_throttle_sp, float max_throttle_sp)
+  {
+
+    min_throttle = min_throttle_sp;
+    max_throttle = max_throttle_sp;
+
+    return *this;
+  }
+
+  FwControlLimits & withMaxAcceleration(float max_lateral_acceleration_sp)
+  {
+
+    max_lateral_acceleration = max_lateral_acceleration_sp;
+    return *this;
+  }
+
+  FwControlLimits & withTargetSinkRate(float target_sink_rate_sp)
+  {
+
+    target_sink_rate = target_sink_rate_sp;
+    return *this;
+  }
+
+  FwControlLimits & withTargetClimbRate(float target_climb_rate_sp)
+  {
+
+    target_climb_rate = target_climb_rate_sp;
+    return *this;
+  }
 };
 
 /** @}*/
--- px4_ros2_cpp/src/control/setpoint_types/experimental/fixedwing_lateral_longitudinal.cpp
+++ px4_ros2_cpp/src/control/setpoint_types/experimental/fixedwing_lateral_longitudinal.cpp
@@ -32,52 +32,51 @@ FwLateralLongitudinalSetpointType::FwLateralLongitudinalSetpointType(Context & c
 }
 
 void FwLateralLongitudinalSetpointType::update(
-  const float course_setpoint,
-  const float airspeed_direction,
-  const float lateral_acceleration_setpoint,
-  const float altitude_setpoint_msl,
-  const float height_rate_setpoint,
-  const float equivalent_airspeed_setpoint,
-  const std::optional<float> & min_pitch,
-  const std::optional<float> & max_pitch,
-  const std::optional<float> & min_throttle,
-  const std::optional<float> & max_throttle,
-  const std::optional<float> & max_lat_acc,
-  const std::optional<float> & target_climb_rate,
-  const std::optional<float> & target_sink_rate)
+  const FwLateralLongitudinalSetpoint & setpoint,
+  const FwControlLimits & limits)
 {
   onUpdate();
 
-  px4_msgs::msg::FixedWingLateralSetpoint lateral_sp{};
-  lateral_sp.course = course_setpoint;
-  lateral_sp.airspeed_direction = airspeed_direction;
-  lateral_sp.lateral_acceleration = lateral_acceleration_setpoint;
-
-  _fw_lateral_sp_pub->publish(lateral_sp);
-
-  px4_msgs::msg::FixedWingLongitudinalSetpoint longitudinal_sp{};
-  longitudinal_sp.altitude = altitude_setpoint_msl;
-  longitudinal_sp.height_rate = height_rate_setpoint;
-  longitudinal_sp.equivalent_airspeed = equivalent_airspeed_setpoint;
-  longitudinal_sp.pitch_direct = NAN;
-  longitudinal_sp.throttle_direct = NAN;
-
-  _fw_longitudinal_sp_pub->publish(longitudinal_sp);
+  update(setpoint);
 
   px4_msgs::msg::LateralControlLimits lateral_limits{};
-  lateral_limits.lateral_accel_max = max_lat_acc.value_or(NAN);
+  lateral_limits.lateral_accel_max = limits.max_lateral_acceleration.value_or(NAN);
 
   _lateral_control_limits_pub->publish(lateral_limits);
 
   px4_msgs::msg::LongitudinalControlLimits longitudinal_limits{};
-  longitudinal_limits.pitch_min = min_pitch.value_or(NAN);
-  longitudinal_limits.pitch_max = max_pitch.value_or(NAN);
-  longitudinal_limits.throttle_min = min_throttle.value_or(NAN);
-  longitudinal_limits.throttle_max = max_throttle.value_or(NAN);
-  longitudinal_limits.climb_rate_target = target_climb_rate.value_or(NAN);
-  longitudinal_limits.sink_rate_target = target_sink_rate.value_or(NAN);
+  longitudinal_limits.pitch_min = limits.min_pitch.value_or(NAN);
+  longitudinal_limits.pitch_max = limits.max_pitch.value_or(NAN);
+  longitudinal_limits.throttle_min = limits.min_throttle.value_or(NAN);
+  longitudinal_limits.throttle_max = limits.max_throttle.value_or(NAN);
+  longitudinal_limits.equivalent_airspeed_min = limits.min_equivalent_airspeed.value_or(NAN);
+  longitudinal_limits.equivalent_airspeed_max = limits.max_equivalent_airspeed.value_or(NAN);
+  longitudinal_limits.climb_rate_target = limits.target_climb_rate.value_or(NAN);
+  longitudinal_limits.sink_rate_target = limits.target_sink_rate.value_or(NAN);
 
   _longitudinal_control_limits_pub->publish(longitudinal_limits);
+
+}
+
+void FwLateralLongitudinalSetpointType::update(const FwLateralLongitudinalSetpoint & setpoint)
+{
+  onUpdate();
+
+  px4_msgs::msg::FixedWingLateralSetpoint lateral_sp{};
+  lateral_sp.course = setpoint.course.value_or(NAN);
+  lateral_sp.airspeed_direction = setpoint.airspeed_direction.value_or(NAN);
+  lateral_sp.lateral_acceleration = setpoint.lateral_acceleration.value_or(NAN);
+
+  _fw_lateral_sp_pub->publish(lateral_sp);
+
+  px4_msgs::msg::FixedWingLongitudinalSetpoint longitudinal_sp{};
+  longitudinal_sp.altitude = setpoint.altitude_msl.value_or(NAN);
+  longitudinal_sp.height_rate = setpoint.height_rate.value_or(NAN);
+  longitudinal_sp.equivalent_airspeed = setpoint.equivalent_airspeed.value_or(NAN);
+  longitudinal_sp.pitch_direct = NAN;
+  longitudinal_sp.throttle_direct = NAN;
+
+  _fw_longitudinal_sp_pub->publish(longitudinal_sp);
 }
 
 SetpointBase::Configuration FwLateralLongitudinalSetpointType::getConfiguration()
