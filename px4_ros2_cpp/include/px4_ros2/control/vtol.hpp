48274011a8e759bbe4d889a3ac2a3b8d5732fd78
mahima-yoga <mahima@auterion.com>
2025-04-09 11:45:34 +0200

    vtol: add time check for publishing vtol transition command

--- px4_ros2_cpp/include/px4_ros2/control/vtol.hpp
+++ px4_ros2_cpp/include/px4_ros2/control/vtol.hpp
@@ -9,60 +9,59 @@
  #include <px4_msgs/msg/vehicle_status.hpp>
  #include <px4_msgs/msg/vtol_vehicle_status.hpp>
  #include <Eigen/Core>
- 
+
  #include <px4_ros2/common/setpoint_base.hpp>
- 
- namespace px4_ros2
- {
- /** \ingroup control
+
+namespace px4_ros2
+{
+/** \ingroup control
   *  @{
   */
- /**
+/**
   * @brief Provides VTOL transition command
   *
   */
- class VTOL
- {
- public:
- 
-   explicit VTOL(Context & context);
+class VTOL
+{
+public:
+  explicit VTOL(Context & context);
 
-   enum class State {
-    UNDEFINED = 0, 
-    TRANSITION_TO_FIXED_WING = 1, 
-    TRANSITION_TO_MULTICOPTER = 2, 
-    MULTICOPTER = 3, 
+  enum class State
+  {
+    UNDEFINED = 0,
+    TRANSITION_TO_FIXED_WING = 1,
+    TRANSITION_TO_MULTICOPTER = 2,
+    MULTICOPTER = 3,
     FIXED_WING  = 4
-   }; 
-   
-   /**
-    * VTOL Transition
-    */
+  };
 
-   void to_multicopter(); 
-   void to_fixedwing();
+  /**
+   * VTOL Transition
+   */
 
-   VTOL::State get_current_state(); 
-   
-   private:
+  void to_multicopter();
+  void to_fixedwing();
 
-   rclcpp::Node & _node;
-   rclcpp::Publisher<px4_msgs::msg::VehicleCommand>::SharedPtr _vehicle_command_pub;
-   rclcpp::Subscription<px4_msgs::msg::VehicleStatus>::SharedPtr _vehicle_status_sub; 
-   rclcpp::Subscription<px4_msgs::msg::VtolVehicleStatus>::SharedPtr _vtol_vehicle_status_sub; 
+  VTOL::State get_current_state();
+
+private:
+  rclcpp::Node & _node;
+  rclcpp::Publisher<px4_msgs::msg::VehicleCommand>::SharedPtr _vehicle_command_pub;
+  rclcpp::Subscription<px4_msgs::msg::VehicleStatus>::SharedPtr _vehicle_status_sub;
+  rclcpp::Subscription<px4_msgs::msg::VtolVehicleStatus>::SharedPtr _vtol_vehicle_status_sub;
+  rclcpp::Time _last_update{};
 
 
-   px4_msgs::msg::VehicleStatus::UniquePtr _vehicle_status_msg;
-   px4_msgs::msg::VtolVehicleStatus::UniquePtr _vtol_vehicle_status_msg; 
+  px4_msgs::msg::VehicleStatus::UniquePtr _vehicle_status_msg;
+  px4_msgs::msg::VtolVehicleStatus::UniquePtr _vtol_vehicle_status_msg;
 
-   uint _system_id; 
-   uint _component_id; 
-   uint _vehicle_type; 
-   bool _is_vtol; 
+  uint _system_id;
+  uint _component_id;
+  uint _vehicle_type;
+  bool _is_vtol;
 
-   VTOL::State _current_state{VTOL::State::UNDEFINED}; 
- };
- 
- /** @}*/
- } /* namespace px4_ros2 */
- 
+  VTOL::State _current_state{VTOL::State::UNDEFINED};
+};
+
+/** @}*/
+}  /* namespace px4_ros2 */
--- px4_ros2_cpp/src/control/vtol.cpp
+++ px4_ros2_cpp/src/control/vtol.cpp
@@ -7,112 +7,119 @@
  #include <px4_ros2/utils/message_version.hpp>
  #include <rclcpp/rclcpp.hpp>
 
- 
- using namespace std::chrono_literals;
- 
- namespace px4_ros2
- {
- 
- VTOL::VTOL(Context & context)
- : _node(context.node())
- {
-    _vehicle_command_pub = _node.create_publisher<px4_msgs::msg::VehicleCommand>(
+
+using namespace std::chrono_literals;
+
+namespace px4_ros2
+{
+
+VTOL::VTOL(Context & context)
+: _node(context.node())
+{
+  _vehicle_command_pub = _node.create_publisher<px4_msgs::msg::VehicleCommand>(
     context.topicNamespacePrefix() + "fmu/in/vehicle_command" + px4_ros2::getMessageNameVersion<px4_msgs::msg::VehicleCommand>(),
     1);
-    _vehicle_status_sub  = _node.create_subscription<px4_msgs::msg::VehicleStatus>(
+
+  _vehicle_status_sub = _node.create_subscription<px4_msgs::msg::VehicleStatus>(
     context.topicNamespacePrefix() + "fmu/out/vehicle_status" + px4_ros2::getMessageNameVersion<px4_msgs::msg::VehicleStatus>(),
-    rclcpp::QoS(10).best_effort(), 
+    rclcpp::QoS(10).best_effort(),
     [this](px4_msgs::msg::VehicleStatus::UniquePtr msg) {
-        _system_id = msg->system_id; 
-        _component_id = msg->component_id; 
-        _vehicle_type = msg->vehicle_type;
-        _is_vtol = msg->is_vtol; 
+      _system_id = msg->system_id;
+      _component_id = msg->component_id;
+      _vehicle_type = msg->vehicle_type;
+      _is_vtol = msg->is_vtol;
 
     });
 
-    _vtol_vehicle_status_sub  = _node.create_subscription<px4_msgs::msg::VtolVehicleStatus>(
+  _vtol_vehicle_status_sub = _node.create_subscription<px4_msgs::msg::VtolVehicleStatus>(
     context.topicNamespacePrefix() + "fmu/out/vtol_vehicle_status" + px4_ros2::getMessageNameVersion<px4_msgs::msg::VtolVehicleStatus>(),
-    rclcpp::QoS(10).best_effort(), 
+    rclcpp::QoS(10).best_effort(),
     [this](px4_msgs::msg::VtolVehicleStatus::UniquePtr msg) {
 
-        switch (msg->vehicle_vtol_state)
-        {
+      switch (msg->vehicle_vtol_state) {
         case px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_TRANSITION_TO_FW:
-            _current_state = VTOL::State::TRANSITION_TO_FIXED_WING; 
-            break;
+          _current_state = VTOL::State::TRANSITION_TO_FIXED_WING;
+          break;
         case px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_TRANSITION_TO_MC:
-            _current_state = VTOL::State::TRANSITION_TO_MULTICOPTER; 
-            break; 
+          _current_state = VTOL::State::TRANSITION_TO_MULTICOPTER;
+          break;
         case px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_MC:
-            _current_state = VTOL::State::MULTICOPTER; 
-            break; 
+          _current_state = VTOL::State::MULTICOPTER;
+          break;
         case px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_FW:
-            _current_state = VTOL::State::FIXED_WING; 
-            break; 
+          _current_state = VTOL::State::FIXED_WING;
+          break;
         default:
-            _current_state = VTOL::State::UNDEFINED; 
-        }
-        
+          _current_state = VTOL::State::UNDEFINED;
+      }
+
     });
- }
 
- VTOL::State VTOL::get_current_state(){
-    return _current_state; 
+  _last_update = _node.get_clock()->now();
 }
- 
- void VTOL::to_multicopter()
- {
-    if(_is_vtol){
-        if(_vehicle_type == px4_msgs::msg::VehicleStatus::VEHICLE_TYPE_FIXED_WING){
 
-
-            px4_msgs::msg::VehicleCommand cmd;
-            
-            cmd.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_VTOL_TRANSITION; 
-            cmd.param1 = px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_MC;
-
-            cmd.param2 = 0;
-    
-            cmd.target_system = _system_id; 
-            cmd.target_component = _component_id; 
-    
-            _vehicle_command_pub->publish(cmd);
-        } 
-        else if(_vehicle_type == px4_msgs::msg::VehicleStatus::VEHICLE_TYPE_ROTARY_WING){
-
-            RCLCPP_WARN(_node.get_logger(), "VTOL already in hover state.");
-        }
-    }
-    else{
-        RCLCPP_WARN(_node.get_logger(), "VTOL Transition not supported by current vehicle type.");
-    }
- }
-
- void VTOL::to_fixedwing()
- {
-    if(_is_vtol){
-        if(_vehicle_type == px4_msgs::msg::VehicleStatus::VEHICLE_TYPE_ROTARY_WING){
-
-            px4_msgs::msg::VehicleCommand cmd;
-            
-            cmd.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_VTOL_TRANSITION; 
-            cmd.param1 = px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_FW;
-
-            cmd.param2 = 0;
-    
-            cmd.target_system = _system_id; 
-            cmd.target_component = _component_id; 
-    
-            _vehicle_command_pub->publish(cmd);
-        }
-        else if(_vehicle_type == px4_msgs::msg::VehicleStatus::VEHICLE_TYPE_FIXED_WING){
-
-            RCLCPP_WARN(_node.get_logger(), "VTOL already in fixed-wing state.");
-        }    
-        else{
-            RCLCPP_WARN(_node.get_logger(), "VTOL Transition not supported by current vehicle type.");
-        }
-    }
+VTOL::State VTOL::get_current_state()
+{
+  return _current_state;
 }
-} // namespace px4_ros2
- 
+
+void VTOL::to_multicopter()
+{
+  if (_is_vtol) {
+    const auto now = _node.get_clock()->now();
+
+    if (_vehicle_type == px4_msgs::msg::VehicleStatus::VEHICLE_TYPE_FIXED_WING &&
+      (now - _last_update) > 150ms)
+    {
+      _last_update = now;
+
+      px4_msgs::msg::VehicleCommand cmd;
+
+      cmd.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_VTOL_TRANSITION;
+      cmd.param1 = px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_MC;
+
+      cmd.param2 = 0;
+
+      cmd.target_system = _system_id;
+      cmd.target_component = _component_id;
+
+      _vehicle_command_pub->publish(cmd);
+    } else if (_vehicle_type == px4_msgs::msg::VehicleStatus::VEHICLE_TYPE_ROTARY_WING) {
+
+      RCLCPP_WARN(_node.get_logger(), "VTOL already in hover state.");
+    }
+  } else {
+    RCLCPP_WARN(_node.get_logger(), "VTOL Transition not supported by current vehicle type.");
+  }
+}
+
+void VTOL::to_fixedwing()
+{
+  if (_is_vtol) {
+    const auto now = _node.get_clock()->now();
+    if (_vehicle_type == px4_msgs::msg::VehicleStatus::VEHICLE_TYPE_ROTARY_WING &&
+      (now - _last_update) > 150ms)
+    {
+      _last_update - now;
+
+      px4_msgs::msg::VehicleCommand cmd;
+
+      cmd.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_VTOL_TRANSITION;
+      cmd.param1 = px4_msgs::msg::VtolVehicleStatus::VEHICLE_VTOL_STATE_FW;
+
+      cmd.param2 = 0;
+
+      cmd.target_system = _system_id;
+      cmd.target_component = _component_id;
+
+      _vehicle_command_pub->publish(cmd);
+    } else if (_vehicle_type == px4_msgs::msg::VehicleStatus::VEHICLE_TYPE_FIXED_WING) {
+
+      RCLCPP_WARN(_node.get_logger(), "VTOL already in fixed-wing state.");
+    }
+  } else {
+    RCLCPP_WARN(_node.get_logger(), "VTOL Transition not supported by current vehicle type.");
+  }
+}
+}
+// namespace px4_ros2
