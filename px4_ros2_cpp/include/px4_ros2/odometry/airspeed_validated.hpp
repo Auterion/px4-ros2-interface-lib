b0176e18659bcb618db1cfcd28f1edc5e75091b0
mahima-yoga <mahima@auterion.com>
2025-04-29 14:25:27 +0200

    odometry: add OdometryAirspeedValidated class

---
+++ px4_ros2_cpp/include/px4_ros2/odometry/airspeed_validated.hpp
@@ -0,0 +1,53 @@
+/****************************************************************************
+ * Copyright (c) 2025 PX4 Development Team.
+ * SPDX-License-Identifier: BSD-3-Clause
+ ****************************************************************************/
+
+#pragma once
+
+#include <Eigen/Eigen>
+#include <px4_msgs/msg/airspeed_validated.hpp>
+#include <px4_ros2/common/context.hpp>
+#include <px4_ros2/utils/subscription.hpp>
+
+namespace px4_ros2
+{
+/** \ingroup odometry
+ *  @{
+ */
+
+/**
+ * @brief Provides access to the vehicle's airspeed estimates
+ */
+class OdometryAirspeedValidated : public Subscription<px4_msgs::msg::AirspeedValidated>
+{
+public:
+  explicit OdometryAirspeedValidated(Context & context);
+
+  float indicatedAirspeed() const
+  {
+    const px4_msgs::msg::AirspeedValidated & aspd = last();
+    return aspd.indicated_airspeed_m_s;
+  }
+
+  float calibratedAirspeed() const
+  {
+    const px4_msgs::msg::AirspeedValidated & aspd = last();
+    return aspd.calibrated_airspeed_m_s;
+  }
+
+  float trueAirspeed() const
+  {
+    const px4_msgs::msg::AirspeedValidated & aspd = last();
+    return aspd.true_airspeed_m_s;
+  }
+
+  bool airspeedSensorMeasurementValid() const
+  {
+    const px4_msgs::msg::AirspeedValidated & aspd = last();
+    return aspd.airspeed_sensor_measurement_valid;
+  }
+};
+
+/** @}*/
+} /* namespace px4_ros2 */
